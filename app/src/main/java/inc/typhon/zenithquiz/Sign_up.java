0211acd56a8a4ba7464acdc919555facad245d2c
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-11-07 20:29:16 +0600

    Sign_up.java

---
+++ app/src/main/java/inc/typhon/zenithquiz/Sign_up.java
@@ -0,0 +1,14 @@
+package inc.typhon.zenithquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up2);
+    }
+}
