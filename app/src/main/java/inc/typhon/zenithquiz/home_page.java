a126a50595865ab1ca82c8f79b032a1f07d0d16c
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-11-11 15:04:57 +0600

    idk us us

--- app/src/main/java/inc/typhon/zenithquiz/home_page.java
+++ app/src/main/java/inc/typhon/zenithquiz/home_page.java
@@ -2,11 +2,13 @@ package inc.typhon.zenithquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.bumptech.glide.Glide;
@@ -17,11 +19,15 @@ public class home_page extends AppCompatActivity {
    LinearLayout linearLayout;
    ImageView user_image;
    int SCREEN_HEIGHT, SCREEN_WIDTH;
+   TextView user_name;
+   SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+
         lottieAnimationView = findViewById(R.id.lottieAnimationView);
         lottieAnimationView.setAnimation(R.raw.guy_on_laptop);
         lottieAnimationView.playAnimation();
@@ -31,19 +37,23 @@ public class home_page extends AppCompatActivity {
         message = findViewById(R.id.message_button);
         linearLayout= findViewById(R.id.linearLayout2);
         user_image = findViewById(R.id.user_image);
+        user_name = findViewById(R.id.user_name_in_home_page);
+
+
         SCREEN_HEIGHT = getResources().getDisplayMetrics().heightPixels;
         SCREEN_WIDTH = getResources().getDisplayMetrics().widthPixels;
         quiz.setHeight((int) (SCREEN_HEIGHT/6.2));
         web.setHeight((int) (SCREEN_HEIGHT/6.2));
         blog.setHeight((int) (SCREEN_HEIGHT/6.2));
         message.setHeight((int) (SCREEN_HEIGHT/6.2));
+        sharedPreferences = getSharedPreferences("user",MODE_PRIVATE);
         //use glide to set image from url circularly
         Glide.with(this)
                 .load(R.drawable.user_image)
                 .circleCrop()
                 .into(user_image);
 
-
+        user_name.setText(sharedPreferences.getString("name",""));
 
 
 
