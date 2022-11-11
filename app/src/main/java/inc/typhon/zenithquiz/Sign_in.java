f04d668a1ca019a64dfe9d572c2879753ec25f7a
Md Kaif Ibn Zaman <mibnzaman02@gmail.com>
2022-11-11 16:09:18 +0600

    idk us us

--- app/src/main/java/inc/typhon/zenithquiz/Sign_in.java
+++ app/src/main/java/inc/typhon/zenithquiz/Sign_in.java
@@ -6,18 +6,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Sign_in extends AppCompatActivity {
     Context context ;
+    EditText email,password;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
         context = this;
+        firebaseAuth = FirebaseAuth.getInstance();
     }
     public void onSignInClick(View view) {
-        Intent intent = new Intent(context, home_page.class);
-        startActivity(intent);
+       firebaseAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(task -> {
+           if (task.isSuccessful()){
+               startActivity(new Intent(context,home_page.class));
+           }
+       });
     }
 }
